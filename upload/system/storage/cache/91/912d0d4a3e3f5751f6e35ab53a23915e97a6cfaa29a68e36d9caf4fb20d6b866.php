b5cff87514d39766e5d2ff3e8fc57262ee3161a5
Morakot Thongprom <tonmorakot@gmail.com>
2020-02-10 21:27:54 +0700

    1.0.0

---
+++ upload/system/storage/cache/91/912d0d4a3e3f5751f6e35ab53a23915e97a6cfaa29a68e36d9caf4fb20d6b866.php
@@ -0,0 +1,52 @@
+<?php
+
+/* default/template/common/search.twig */
+class __TwigTemplate_d28ebc10839dd92433bf5c98c7d21eab7e473353aa85a11a1d3fb6f834ed7b23 extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        // line 1
+        echo "<div id=\"search\" class=\"input-group\">
+  <input type=\"text\" name=\"search\" value=\"";
+        // line 2
+        echo (isset($context["search"]) ? $context["search"] : null);
+        echo "\" placeholder=\"";
+        echo (isset($context["text_search"]) ? $context["text_search"] : null);
+        echo "\" class=\"form-control input-lg\" />
+  <span class=\"input-group-btn\">
+    <button type=\"button\" class=\"btn btn-default btn-lg\"><i class=\"fa fa-search\"></i></button>
+  </span>
+</div>";
+    }
+
+    public function getTemplateName()
+    {
+        return "default/template/common/search.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  22 => 2,  19 => 1,);
+    }
+}
+/* <div id="search" class="input-group">*/
+/*   <input type="text" name="search" value="{{ search }}" placeholder="{{ text_search }}" class="form-control input-lg" />*/
+/*   <span class="input-group-btn">*/
+/*     <button type="button" class="btn btn-default btn-lg"><i class="fa fa-search"></i></button>*/
+/*   </span>*/
+/* </div>*/
